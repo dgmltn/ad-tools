7224dff547e6729974378a3ea50c53a824c8d6ff
Brennan Taylor <brennan@mobiata.com>
2013-08-15 14:43:27 -0700

    ad dexstats now takes an apk instead of a dex file

--- src/dex.sh
+++ src/dex.sh
@@ -2,7 +2,7 @@ function usage_dexstats() {
 cat << EOF
 usage: ad dexstats <classes.dex>
 
-Prints total method count as well as a breakdown of method counts per package
+Prints a breakdown of method counts per package
 
 Taken from gist posted by Jake Wharton (https://gist.github.com/JakeWharton)
 
@@ -16,21 +16,31 @@ function dex_method_count() {
 
 function dexstats() {
     exactly dexstats 1 $#
-    dexfile="$1"
-    check_file "$dexfile"
-
-    ensure hexdump
-    count=$(dex_method_count "$dexfile")
-    echo -e "$count\tTotal Methods"
+    apk="$1"
+    check_file "$apk"
 
     ensure smali baksmali
-    dir=$(mktemp -d -t dex)
+
+    dir=$(mktemp -d -t ad-tools-dexstats)
+    dexfile=$(mktemp -t ad-tools-classes.dex)
+
+
+    # Extract entire classes.dex
+    unzip -p "$apk" "classes.dex" | cat > $dexfile
+
+    # Recompile into only the leaf packages
     baksmali $dexfile -o $dir
-    for pkg in `find $dir/* -type d`; do
+    for pkg in `find_leaves $dir/*`; do
         smali $pkg -o $pkg/classes.dex
         count=$(dex_method_count $pkg/classes.dex)
         name=$(echo ${pkg:(${#dir} + 1)} | tr '/' '.')
         echo -e "$count\t$name"
     done
+
+    # Recompile the entire thing so dex_method_count works
+    smali $dir -o $dir/classes.dex
+    totalcount=$(dex_method_count $dir/classes.dex)
+    echo "Total: ${totalcount}"
     rm -rf $dir
+    rm -rf $dexfile
 }
